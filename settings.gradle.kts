ccb606ed86da12f7cd3392facc274f6072fa7eda
BartoszewskiA <bartoszewski.a@outlook.com>
2023-11-16 12:43:03 +0100

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "lan01p01"
+include(":app")
